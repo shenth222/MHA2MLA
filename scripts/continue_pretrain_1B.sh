0c33e5d03e65ca946241be86762750d1636c1323
bg51717 <2419163999@qq.com>
2025-01-28 17:38:33 +0800

    impl the low rank v

--- scripts/continue_pretrain_1B.sh
+++ scripts/continue_pretrain_1B.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 #################### 环境变量 ####################
-export CUDA_VISIBLE_DEVICES="1,2"
+export CUDA_VISIBLE_DEVICES=""
 export PYTHONPATH=..:$PYTHONPATH
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 export MASTER_PORT="auto"
@@ -11,11 +11,9 @@ export WANDB_PROJECT="smollm_nt"
 #################### 任务执行 ####################
 
 
-torchrun --nproc_per_node 2 --master_port 24564 \
+torchrun --nproc_per_node 16 --master_port 24575 \
     -m src.run_train \
-    --config-file ../configs/continue_pretraining/360M_1.yaml \
+    --config-file ../configs/continue_pretraining/135M_lr_1e-4_cooldown_10.yaml \
     --rope-cfg ../configs/continue_pretraining/rope_v0.yaml
 
-feishu_msg -u 18055481550 -m 'rope_v0'
-
-./eval_partial_rope_all_64.sh
+./eval_all_1B.sh
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 #################### 环境变量 ####################
-export CUDA_VISIBLE_DEVICES='0'
+export CUDA_VISIBLE_DEVICES='2'
 export PYTHONPATH=..:$PYTHONPATH
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 export MASTER_PORT="auto"
@@ -17,18 +17,18 @@ export PYTHONPATH=..:$PYTHONPATH
 # torchrun --nproc_per_node 1 \
 #     -m src.test.test_mla
 
-torchrun --master_port=29564 --nproc_per_node=1 \
-    -m src.conversation.convert_hf_to_nanotron \
-    --checkpoint_path="../checkpoints/rope_v4_topk4_svd_method7_rank8/18000_hf" \
-    --save_path="../checkpoints/test_nt" \
-    --is_mla
+# torchrun --master_port=29564 --nproc_per_node=1 \
+#     -m src.conversation.convert_hf_to_nanotron \
+#     --checkpoint_path="../checkpoints/rope_v4_topk4_svd_method7_rank8/18000_hf" \
+#     --save_path="../checkpoints/test_nt" \
+#     --is_mla
 
-torchrun --master_port=29564 --nproc_per_node=1 \
-    -m src.conversation.convert_nanotron_to_hf \
-    --checkpoint_path="../checkpoints/test_nt" \
-    --tokenizer_name="../checkpoints/rope_v4_topk4_svd_method7_rank8/18000_hf" \
-    --save_path="../checkpoints/test_hf" \
-    --is_mla
+# torchrun --master_port=29564 --nproc_per_node=1 \
+#     -m src.conversation.convert_nanotron_to_hf \
+#     --checkpoint_path="../checkpoints/test_nt" \
+#     --tokenizer_name="../checkpoints/rope_v4_topk4_svd_method7_rank8/18000_hf" \
+#     --save_path="../checkpoints/test_hf" \
+#     --is_mla
 
 
 # torchrun --master_port=29564 --nproc_per_node=1 \
@@ -39,10 +39,14 @@ torchrun --master_port=29564 --nproc_per_node=1 \
 #     --is_mla
 
 
-torchrun --master_port=29564 --nproc_per_node=1 \
-    -m src.test.test_conversation \
-    --config-file "../configs/mla/test.yaml"
+# torchrun --master_port=29564 --nproc_per_node=1 \
+#     -m src.test.test_conversation \
+#     --config-file "../configs/mla/test.yaml"
 
 
 # torchrun --master_port=29564 --nproc_per_node=1 \
 #     -m src.test.dbg_attn_fwd
+
+torchrun --master_port=29564 --nproc_per_node=1 \
+    -m src.low_rank_v.test \
+    --config-file ../configs/low_rank/test.yaml
