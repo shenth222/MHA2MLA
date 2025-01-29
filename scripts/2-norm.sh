54d92a489aacd9fa4ec4b4074621be133416599a
bg51717 <2419163999@qq.com>
2025-01-29 19:39:12 +0800

    upd 1.7B qk_tensor

--- scripts/2-norm.sh
+++ scripts/2-norm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES="0,1,2"
+export CUDA_VISIBLE_DEVICES="7"
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 # export MASTER_PORT="auto"
 export HF_HOME="~/data/hf-home"
@@ -7,4 +7,4 @@ export HF_HOME="~/data/hf-home"
 
 torchrun --nproc_per_node 1 --master_port=25641 \
     ../src/test/test_2_norm.py \
-    --config-file ../configs/test/test_2norm.yaml
+    --config-file ../configs/test/1.7B_2norm.yaml
